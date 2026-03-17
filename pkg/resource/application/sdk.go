65da82f89bb352841b498c9e7deda1af35bb645a
Gustavo Diaz <gustidia@amazon.com>
2026-03-17 22:30:31 +0000

    Run code gen

--- pkg/resource/application/sdk.go
+++ pkg/resource/application/sdk.go
@@ -388,6 +388,12 @@ func (rm *resourceManager) sdkFind(
 	}
 
 	rm.setStatusDefaults(ko)
+	// The EMR Serverless GetApplication API continues to return deleted
+	// applications with their state set to TERMINATED rather than returning
+	// a NotFound error.
+	//
+	// Treating TERMINATED as NotFound allows the reconciler to proceed with
+	// Create if user deletes application externally
 	if ko.Status.State != nil && *ko.Status.State == string(svcapitypes.ApplicationState_TERMINATED) {
 		return nil, ackerr.NotFound
 	}
